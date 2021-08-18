e06a521ff990838adec31d8b4a4fe8e90aeeb3c9
Iamzayed007 <72243831+Iamzayed007@users.noreply.github.com>
2021-08-18 17:40:31 +0600

    commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="bg-blue-800 text-center p-4 text-white mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>
+      Covid-19 Tracker
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
