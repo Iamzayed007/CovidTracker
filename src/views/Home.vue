de2a3cd217fc814579f6e9598a5280aa7c23a307
Iamzayed007 <72243831+Iamzayed007@users.noreply.github.com>
2021-08-19 14:21:01 +0600

    final

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,39 @@
 <template>
-  <main v-if="!loading">Show Data</main>
+  <main v-if="!loading">
+    <DateTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="
+        bg-green-700
+        text-white
+        rounded
+        p-3
+        mt-10
+        focus:outline-none
+        hover:bg-green-600
+      "
+    >
+      Clear Country
+    </button>
+  </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
     <img :src="loadingImage" class="w-24 m-auto" alt="" />
   </main>
-  Hello world
 </template>
 
 <script>
+import DateTitle from '@/components/DateTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 export default {
   name: "Home",
-  components: {},
+  components: {
+    DateTitle,DataBoxes,CountrySelect
+  },
   data() {
     return {
       loading: true,
@@ -27,14 +50,27 @@ export default {
       const data = await res.json();
       return data;
     },
+    getCountryData(country){
+      this.stats= country
+      this.title= country.Country 
+    },
+
+    async clearCountryData(){
+
+        this.loading= true
+        const data = await this.fetchCovidData()
+        this.title= 'Global'
+        this.stats= data.Global
+        this.loading= false
+    }
   },
   async created() {
     const data = await this.fetchCovidData();
     console.log(data);
-    this.dateDate = data.Date;
-    this.stats = data.GLobal;
+    this.dataDate = data.Date;
+    this.stats = data.Global;
     this.countries = data.Countries;
-    this.loading = data.false;
+    this.loading = false;
   },
 };
 </script>
